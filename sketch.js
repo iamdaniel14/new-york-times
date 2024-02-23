2114e9c348707638b75c77659ab95fbfd31058e5
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2024-02-23 15:54:54 +0100

    second commit

--- sketch.js
+++ sketch.js
@@ -2,39 +2,52 @@
 //  const url ="https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs"
  
  async function newYorkArticle (){
-const url ="https://api.nytimes.com/svc/topstories/v2/world.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs";
+const url ="https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs";
 
 let response =await  fetch (url);
 let newArticles = await response.json();
-let articleContainer= document.querySelector (".articles-container");
-console.dir (articleContainer);
+
+let headlinesContainer= document.querySelector (".headlines-container");
+console.dir (newArticles);
 
 
-let latestNews=newArticles.results; //checking all the headlines
+let latestNews=newArticles .results; //checking all the headlines
 
 for (let i=0; i<latestNews.length; i++) { 
 
+let newsContainer=document.createElement("div"); //making a new news container for each loop
+newsContainer.classList.add("headline-section")
+// console.log (newsContainer);
+
 let header=document.createElement("h1");
-header.innerHTML= latestNews[i].title;
-articleContainer.appendChild (header);
+header.textContent= latestNews[i].title;
+header.classList.add("headline-title");
+newsContainer.appendChild (header);
 
 let paragraph=document.createElement ("p");
-paragraph.innerHTML=latestNews[i].abstract;
-articleContainer.appendChild (paragraph);
+paragraph.textContent=latestNews[i].abstract;
+paragraph.classList.add("headline-paragraph");
+newsContainer.appendChild (paragraph);
 
 let a=document.createElement("a");
 a.setAttribute ("href",latestNews[i].url);
-a.innerHTML=latestNews[i].title;
-articleContainer.appendChild (a);
+a.textContent=latestNews[i].title;
+a.classList.add ("headline-link");
+newsContainer.appendChild (a);
 
 // console.log (latestNews[i].multimedia[0].url);
-
 let photoCover=document.createElement('img');
 photoCover.setAttribute("src",latestNews[i].multimedia[0].url);
-articleContainer.appendChild(photoCover);
+photoCover.classList.add ("headline-photo-cover")
+newsContainer.appendChild(photoCover);
+
 let published=document.createElement ("p");
-published.innerHTML=latestNews[i].published_date;
-articleContainer.appendChild ( published);
+published.textContent=latestNews[i].published_date;
+published.classList.add("headline-published");
+newsContainer.appendChild ( published);
+
+headlinesContainer.appendChild(newsContainer); //adding new div to a parent div (headlines-container);
+console.log ( headlinesContainer);
 
 }
 }
