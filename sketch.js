f0c84b8ddb1b6ba09e7a8514f334a0ef3fffba7a
iamdaniel14 <96418140+iamdaniel14@users.noreply.github.com>
2024-03-02 18:04:47 +0100

    debugging

--- sketch.js
+++ sketch.js
@@ -4,24 +4,19 @@ let headlinesContainer= document.querySelector (".headlines-container");
 let newsCategory=document.querySelector('#news-category');
 let categoryContainer =document.querySelector('#categoryContainer');
 let categories=["business","sports","technology","obituaries","fashion","realestate"];
-const category="sports";
+let newCategoryTitle=document.querySelector(".category-title");
+ let category="sports";
 
 let url;
-window.onload = () =>{
-url=`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs`;
-fetchTopStories ();
 
-}
+ async function fetchTopStories (category){
 
- async function fetchTopStories (){
-// const url ="https://api.nytimes.com/svc/topstories/v2/sports.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs";
 try{
 
  url=`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs`;
 let response =await  fetch (url);
 let newArticles = await response.json();
-//  return newArticles .results; //checking all the headlines
- displayTopStories(newArticles.results)
+return newArticles.results
 }catch (err){
 console.log ("no data is fetch");
 console.error(err);
@@ -31,10 +26,11 @@ console.error(err);
 
  function displayTopStories(latestNews) { 
 // for (let i=0; i<latestNews.length; i++)
+headlinesContainer.innerHTML = '' //clear existing content
 latestNews.forEach((topStory) => { 
 let newsContainer=document.createElement("div"); //making a new news container for each loop
 newsContainer.classList.add("headline-section");
-// console.log (newsContainer);
+
 
 let header=document.createElement("h1");
 header.textContent= topStory.title;
@@ -46,11 +42,6 @@ paragraph.textContent=topStory.abstract;
 paragraph.classList.add("headline-paragraph");
 newsContainer.appendChild (paragraph);
 
-let a=document.createElement("a");
-a.setAttribute ("href",topStory.url);
-a.textContent=topStory.title;
-a.classList.add ("headline-link");
-newsContainer.appendChild (a);
 
 
 let photoCover=document.createElement('img');
@@ -61,40 +52,54 @@ let published=document.createElement ("p");
 published.textContent=topStory.published_date;
 published.classList.add("headline-published");
 newsContainer.appendChild ( published);
+
+
+let a=document.createElement("a");
+a.setAttribute ("href",topStory.url);
+a.textContent=topStory.title;
+a.innerHTML="Read more";
+a.classList.add ("headline-link");
+newsContainer.appendChild (a);
+
 headlinesContainer.appendChild(newsContainer); //adding new div to a parent div (headlines-container);
 })
 
 }
 
-//define the function a execute it immediately with ()
-//  (async ()=> {
-//     try{
-//      latestNews= await (fetchTopStories ())
-//     displayTopStories(latestNews);
-//     } catch(err){
-//     console.log("failed to fetch random stories");
-//     console.error(err);
-//     }
-// })();
-
-const changingCategory=(e,category)=>{
+function changingCategory(event,newCategory){
 let newsCategory=document.querySelectorAll (".category-option");
+
 newsCategory.forEach((element)=>{
 element.classList.remove('active')
 });
 
- url=`https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=m2el98Ix9bYGsagBRzWNVuH9ysGouuSs`;
- e.target.classList.add("active");
-  fetchTopStories ();
+event.target.classList.add("active");
+
+category= newCategory; //changing the global category
+showNews  (category); //calling new with updated category
+
+
+newCategoryTitle.innerHTML=newCategory;  //changing the new category according to the one that is clicked
 
 }
 
 function Category(){
-categoryContainer .innerHTML
-for (let category of categories){
-categoryContainer.innerHTML+=`<button class="category-option ${category=="sports" ? "active":""}" onclick="changingCategory(event,'${category}')">${category} </button>`
-console.log(categoryContainer);
- }
+categories.forEach((category)=> { 
+categoryContainer.innerHTML+=`<button class="category-option ${category=="sports" ? "active":""}" onclick="changingCategory(event,'${category}')">${category} </button>`;
+
+ })
 }
 
+async function showNews (category){
+  let news=await fetchTopStories(category);
+  displayTopStories(news);
+}
+
+
+
+
+
 Category();
+showNews (category)
+
+
